2bd898e14413420974192aa7d1a7921f77ea1df8
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-05 01:24:48 +0900

    gone /party_match_link

--- controllers/lfg.rb
+++ controllers/lfg.rb
@@ -1,8 +1,4 @@
-class LfgController < Sinatra::Base
-  configure do
-    set :webhook, ENV['LFG_HOOK']    
-  end
-  
+class LfgController < Sinatra::Base  
   get '/party_match_info' do
     headers 'Access-Control-Allow-Origin' => '*'
     json({
@@ -32,61 +28,7 @@ class LfgController < Sinatra::Base
     })
   end
 
-  post '/party_match_link' do  
-    validates do
-      configure do
-        def self.messages
-          super.merge(
-            en: {errors: {
-              check_spam_user: 'he is spam user',
-              check_spam_message: 'message contains spam word'
-            }}
-          )
-        end
-      end
-    
-      required(:playerId).filled(:int?, gt?: 0)
-      required(:lfg).schema do
-        required(:id).filled(:int?, gt?: 0)
-        required(:unk2).value(eql?: 65)
-        required(:name).filled(:str?)
-        required(:message).filled(:str?)
-
-        validate(check_spam_user: :name) do |name|
-          name.normalize != 'あるていしあ' &&
-          name.normalize !~ /^知.+ちゃん$/
-        end
-
-        validate(check_spam_message: :message) do |_|
-          message = _.normalize
-          ['砲火', '名誉', '闘志', '回収', '海賊'].none?{|word| message.include?(word)}
-        end
-      end
-    end
-
-    lfg = params[:lfg]
-    settings.mutex.synchronize do
-      last_pr = settings.cache.get(lfg['id'])
-      halt 429 if last_pr && lfg['message'] == last_pr
-      settings.cache.set(lfg['id'], lfg['message'], 180)
-    end
-
-    color = lfg['raid'] == 0 ? 0x54a0ff : 0xfeca57
-    color = 0xee5253 if lfg['message'] =~ /買い?取/
-
-    HTTP.post(settings.webhook, json: {
-      embeds: [{
-        author: {
-          name: lfg['name']
-        },
-        description: lfg['message'],
-        color: color,
-        timestamp: Time.now.iso8601
-      }]
-    })
-
-    json ({
-      success: 'updated'
-    })
+  post '/party_match_link' do
+    halt 410
   end
 end

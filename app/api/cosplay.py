9d78a68ab4044d13626dfe016c8e78967e3422b5
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-19 22:21:02 +0900

    add cosplayer server

---
+++ app/api/cosplay.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from . import api
+from ..models import Cosplay
+from ..response import success_jsonify
+
+from flask import request
+
+@api.route('/cosplay/list', methods=['GET'])
+def cosplay_list():
+  presets = Cosplay.query().fetch()
+  return success_jsonify({cosplay.name: cosplay.preset for cosplay in presets})
+
+@api.route('/cosplay/upload', methods=['POST'])
+def cosplay_upload():
+  data = request.get_json()
+  for k, v in data.items():
+    first = Cosplay.query(Cosplay.name == k).get()
+    if first:
+      first.preset = v
+    else:
+      first = Cosplay(name=k, preset=v)
+
+    first.put()
+
+  return success_jsonify({
+    'success': 'updated'
+  })

61f0f123e650fc8e30a578d7966d8bb83a0cea7a
valkyr1e <nkpoidjp+tera@gmail.com>
2018-09-21 04:43:38 +0900

    add error handling

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 from api import api as api_blueprint
+from response import error_jsonify
 app.register_blueprint(api_blueprint)
+
+@app.errorhandler(500)
+def internal_error(e):
+  return error_jsonify({
+    'error': 'エラーが発生しました'
+  })

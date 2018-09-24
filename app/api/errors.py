685384998744baaf47c551dfd400e4cda3229736
valkyr1e <42307360+valkyr1e-tera@users.noreply.github.com>
2018-09-24 11:00:06 +0900

    Feature/character search (#9)
    * fix
    
    * impl
    
    * yapf
    
    * add error code

--- app/api/errors.py
+++ app/api/errors.py
@@ -8,3 +8,13 @@ from ..exceptions import *
 @api.errorhandler(OutOfTimeException)
 def out_of_time(e):
     return error_jsonify({'error': 'out of time'})
+
+
+@api.errorhandler(UserNotFound)
+def funcname(e):
+    return error_jsonify({'error': 'user not found'}, code=404)
+
+
+@api.errorhandler(CharacterNotFound)
+def funcname(e):
+    return error_jsonify({'error': 'character not found'}, code=404)
